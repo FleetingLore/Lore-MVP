a603b71e93397e0e60aeaa8b4905153de765ab7a
Fleeting <Macro__9@outlook.com>
2025-09-24 13:49:36 +0800

    test

---
+++ Lir/target/debug/deps/Lir-453450da71ea9b16.d
@@ -0,0 +1,5 @@
+D:\code\Lore\Lir\target\debug\deps\Lir-453450da71ea9b16.d: src\lib.rs
+
+D:\code\Lore\Lir\target\debug\deps\libLir-453450da71ea9b16.rmeta: src\lib.rs
+
+src\lib.rs:
---
+++ Lir/target/debug/deps/Lir-7acdf0b325d251f6.d
@@ -0,0 +1,5 @@
+D:\code\Lore\Lir\target\debug\deps\Lir-7acdf0b325d251f6.d: src\lib.rs
+
+D:\code\Lore\Lir\target\debug\deps\libLir-7acdf0b325d251f6.rmeta: src\lib.rs
+
+src\lib.rs:
